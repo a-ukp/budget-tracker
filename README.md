9bffdfbf8a0ff1abd054161fe97bdc665d5173d8
Anna <uyenkphung@gmail.com>
2021-07-19 10:29:42 -0500

    added preview

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Budget Tracker
 
 ## Description
+A budget tracker that you can use online and offline!
 
 ## Usage
+* Git clone this repository in your local folder or visit the link below.
 
 ### Link
 [Budget App](https://aukp-budgetapp.herokuapp.com/)
 
 ### Preview
+![Link Preview](https://cdn.discordapp.com/attachments/862118661091098644/866703103218679888/unknown.png)
--- public/database.js
+++ public/database.js
@@ -4,5 +4,20 @@ const request = window.indexedDB.open('budgetDB', 1);
 request.onupgradeneeded = (event) => {
     db = event.target.result;
     const objStore = db.createObjectStore("BudgetStore", { keyPath: "id", autoIncrement: true });
-    
+
+    objStore.createIndex('name', 'name');
+    objStore.createIndex('date', 'date');
+    objStore.createIndex('value', 'value');
+};
+
+request.onsuccess = (event) => {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDB();
+    }
+};
+
+(checkDB) => {
+    var 
 }
